a5970fb8480c67c2fc1c12f394726f6f2e0aaa13
Yvan Yang <yvan.yang@ulsee.com>
2018-07-19 19:27:24 +0800

    first commit

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+import Header from './components/Header'
+import Login from './components/Login'
+import { Switch, Route } from 'react-router-dom'
+import LinkList from './components/LinkList'
+import CreateLink from './components/CreateLink';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="center w85">
+        <Header />
+        <div className="ph3 pv1 background-gray">
+          <Switch>
+          <Route exact path="/login" component={Login} />
+            <Route exact path="/" component={LinkList} />
+            <Route exact path="/create" component={CreateLink} />
+          </Switch>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
